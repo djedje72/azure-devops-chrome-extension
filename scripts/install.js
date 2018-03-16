a8c4203f07d360088b215501ca1415b5c392c6f6
Rémi FRUTEAU DE LACLOS <remi.fruteau@gmail.com>
2018-03-16 10:50:32 +0100

    fix(install): null pointer

--- scripts/install.js
+++ scripts/install.js
@@ -1,7 +1,8 @@
 chrome.runtime.onInstalled.addListener(({previousVersion}) => {
     switch(previousVersion) {
-        case "1.3.2": {
-            const settings = JSON.parse(localStorage.getItem("settings"));
+        case "1.3.2":
+        case "1.4.1": {
+            const settings = JSON.parse(localStorage.getItem("settings")) || {};
             settings.enableNotifications = true;
             localStorage.setItem("settings", JSON.stringify(settings));
             break;
